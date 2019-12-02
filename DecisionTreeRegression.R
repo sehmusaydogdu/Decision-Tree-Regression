6ffb887266ea2c3dfe53fdfbd8e90cc4edac2c26
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2019-12-02 21:51:03 +0300

    finally homework

--- DecisionTreeRegression.R
+++ DecisionTreeRegression.R
@@ -11,29 +11,11 @@ y_train <- as.matrix(y)[0:150,]
 X_test  <- as.matrix(X)[151:272,]
 y_test  <- as.matrix(y)[151:272,]
 
-# get number of classes and number of features
-K <- max(y)
-D <- ncol(as.matrix(X_train))
-
-# get numbers of train and test samples
-N_train <- length(X_train)
-N_test <- length(X_test)
-
 #pre-pruning parameter
 pre_pruning_values <- list(5,10,15,20,25,30,35,40,45,50)
 rmse_result_list<-c()
 rmse_index <- 0
 P <- 25
-
-#Plot data point
-plot(X_train, y_train, pch = 19,
-     xlim = c(min(X_train), max(X_train)),
-     ylim = c(min(y_train), max(y_train)),
-     xlab = sprintf("Eruption time (min)", P), 
-     ylab = "Waiting time to next encruption (min)", 
-     main = sprintf("P = %g", P),
-     col = "blue")
-points(X_test,y_test, pch = 19,col = "red")
 
 for(p_value in pre_pruning_values){
   P <- p_value
@@ -47,7 +29,7 @@ for(p_value in pre_pruning_values){
   node <- c()
   
   # put all training instances into the root node
-  node_indices <- list(1:N_train)
+  node_indices <- list(1:length(X_train))
   is_terminal <- c(FALSE)
   need_split <- c(TRUE)
   
@@ -82,7 +64,7 @@ for(p_value in pre_pruning_values){
           right_indices <- data_indices[which(X_train[data_indices] >= split_positions[s])]
           split_scores[s] <- sum((y_train[left_indices] - mean(y_train[left_indices]))**2 / length(y_train[data_indices])) +
             sum((y_train[right_indices] - mean(y_train[right_indices]))**2 / length(y_train[data_indices]))
-          -length(right_indices) / length(data_indices) * sum(sapply(1:K, function(c) {mean(y_train[right_indices] == c) * log2(mean(y_train[right_indices] == c))}), na.rm = TRUE)
+          -length(right_indices) / length(data_indices) * sum(sapply(1:max(y), function(c) {mean(y_train[right_indices] == c) * log2(mean(y_train[right_indices] == c))}), na.rm = TRUE)
         }
         score <- min(split_scores)
         split <- split_positions[which.min(split_scores)]
@@ -105,8 +87,8 @@ for(p_value in pre_pruning_values){
   }
   
   # traverse tree for test data points
-  y_predicted <- rep(0, N_test)
-  for (i in 1:N_test) {
+  y_predicted <- rep(0, length(X_test))
+  for (i in 1:length(X_test)) {
     index <- 1
     while (1) {
       if (is_terminal[index] == TRUE) {
@@ -121,6 +103,35 @@ for(p_value in pre_pruning_values){
       }
     }
   }
+  
+  #Plot data point
+  plot(X_train, y_train, pch = 19,
+       xlim = c(min(X_train), max(X_train)),
+       ylim = c(min(y_train), max(y_train)),
+       xlab = sprintf("Eruption time (min)", P), 
+       ylab = "Waiting time to next encruption (min)", 
+       main = sprintf("P = %g", P),
+       col = "blue")
+  points(X_test,y_test, pch = 19,col = "red")
+  data_interval <- seq(from = min(X)-0.1, to = max(X)+0.1, by = 0.01)
+  p_head <- rep(0, length(data_interval))
+  
+  for (i in 1:length(data_interval)) {
+    index <- 1
+    while (1) {
+      if (is_terminal[index] == TRUE) {
+        p_head[i] <- node[index]
+        break
+      } else {
+        if (data_interval[i] <= node_splits[index]) {
+          index <- index * 2
+        } else {
+          index <- index * 2 + 1
+        }
+      }
+    }
+  }
+  lines(data_interval, p_head, type = "l", lwd = 2, col = "black")
   rmse <- sqrt(sum((y_test - y_predicted)**2) / length(y_test))
   print(sprintf("RMSE is %g when P is %g", rmse ,P))
   
@@ -128,7 +139,6 @@ for(p_value in pre_pruning_values){
   rmse_result_list <-c(rmse_result_list,temp)
   rmse_index <- rmse_index+1
 }
-
 
 #Plot pre-pruning and RMSE representation
 plot(pre_pruning_values,rmse_result_list, pch = 19,
